3762c6a66d9b933d27f3af2e038e67c4eba9a00e
MarceloNascc <marcelo.nasccc@gmail.com>
2020-02-22 22:16:59 -0300

    refactor: finalização do css no título

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1 id="title">PROCESSO SELETIVO - LUMEN GAMES</h1>
+        <h1 id="title">PROCESSO SELETIVO - <span className="game">LUMEN GAMES</span></h1>
 
         <div className="btn-group">
           <TransitionButton name="TAREFAS" className="btn" />
